df05e78964ba212eb717c2e8399d724c8f3a3560
steve <stevemighty2030@gmail.com>
2022-08-17 23:13:05 +0300

    the lab is completed

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
+
   // Write your code here!
+  if (someValue > 2500) {
+    return "No can do.";
+   }
+ else if (someValue > 2000) {
+   return "I will gladly take your thirty bucks.";
+   }
+ else {
+     return "This one is on me!";
+   }
+ }
+
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  let cityValue = city === "NYC" ? "Ok, sounds good." : "No go.";
+  return cityValue;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+
+
 
 function switchOnCharmFromTip(){
   // Write your code here!
+  let tip='generous'
+  switch (tip) {
+    case "generous":
+      return 'Thank you so much.';
+      break;
+    case "not as generous":
+      return 'Thank you.';
+      break;
+
+    default:
+      "thanks for everything";
+      return 'Bye.';
+  }
+
 }
